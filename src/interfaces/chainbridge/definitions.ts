2bc789f6b15d0c694c9eb6835b489b0664ab496f
Arvianto Dwi <arvi@ata.network>
2022-02-18 11:10:31 +0700

    fix: eslint errors

--- src/interfaces/chainbridge/definitions.ts
+++ src/interfaces/chainbridge/definitions.ts
@@ -1,20 +1,23 @@
-export default {
-    types: {
-        ResourceId: '[u8; 32]',
-        DepositNonce: 'u64',
-        ProposalStatus: {
-            _enum: [
-                'Initiated',
-                'Approved',
-                'Rejected'
-            ]
-        },
-        ProposalVotes: {
-            votes_for: 'Vec<AccountId>',
-            votes_against: 'Vec<AccountId>',
-            status: 'ProposalStatus'
-        },
-        BridgeTokenId: 'U256',
-        BridgeChainId: 'u8',
+/*
+ * Copyright (c) 2022 automata-network/witness-v2 authors and contributors.
+ * This software may be modified and distributed under the terms of MIT license.
+ * Refer to the LICENSE file for details.
+ */
+const chainBridgeDefs = {
+  types: {
+    ResourceId: '[u8; 32]',
+    DepositNonce: 'u64',
+    ProposalStatus: {
+      _enum: ['Initiated', 'Approved', 'Rejected']
     },
+    ProposalVotes: {
+      votes_for: 'Vec<AccountId>',
+      votes_against: 'Vec<AccountId>',
+      status: 'ProposalStatus'
+    },
+    BridgeTokenId: 'U256',
+    BridgeChainId: 'u8'
+  }
 }
+
+export default chainBridgeDefs
